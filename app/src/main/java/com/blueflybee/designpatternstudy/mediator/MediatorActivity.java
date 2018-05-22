c97a80669fb705f1afd916a205c990aba3610050
shaojun <wusj2017@gmail.com>
2018-05-22 14:14:18 +0800

    实现android mediator

--- app/src/main/java/com/blueflybee/designpatternstudy/mediator/MediatorActivity.java
+++ app/src/main/java/com/blueflybee/designpatternstudy/mediator/MediatorActivity.java
@@ -1,6 +1,7 @@
 package com.blueflybee.designpatternstudy.mediator;
 
 import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,21 +11,13 @@ import com.blueflybee.designpatternstudy.databinding.ActivityMediatorBinding;
 
 public class MediatorActivity extends AppCompatActivity {
 
-  private ActivityMediatorBinding mBinding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    mBinding = DataBindingUtil.setContentView(this, R.layout.activity_mediator);
-
-    ViewMediator viewMediator = new ViewMediator(mBinding);
-    viewMediator.setOnConfirmBtnClickListener(new ViewMediator.OnConfirmBtnClickListener() {
-      @Override
-      public void onClick(View v) {
-        System.out.println("v = " + v);
-      }
-    });
+    ActivityMediatorBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_mediator);
+    ViewMediator viewMediator = new ViewMediator(binding);
 
   }
 
